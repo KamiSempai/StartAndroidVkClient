a29a2a66fab5ea3df9b1acf385025306fc7e38cb
Mezmeraiz <mezmeraiz85@mail.ru>
2015-01-24 17:46:39 +0400

    Набросал авторизацию

---
+++ app/src/main/java/ru/startandroid/vkclient/LoginActivity.java
@@ -0,0 +1,104 @@
+package ru.startandroid.vkclient;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKSdkListener;
+import com.vk.sdk.VKUIHelper;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.dialogs.VKCaptchaDialog;
+
+
+/**
+ * Created by pc on 24.01.2015.
+ */
+public class LoginActivity extends ActionBarActivity {
+
+    private static final String[] scope = {VKScope.MESSAGES,VKScope.FRIENDS};
+    public static final String APP_ID = "4744765";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        VKUIHelper.onCreate(this);
+        //Инициализация SDK - передаем слушатель и id standalone-приложения
+        VKSdk.initialize(listener, APP_ID);
+        //Если token в наличии - запускаем активность, если нет - авторизацию
+        if (VKSdk.wakeUpSession()) {
+            startMainActivity();
+        }else{
+            authorize();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        VKUIHelper.onResume(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Если получен ответ от VKOpenAuthActivity - вызывается один из методов слушателя listener
+        super.onActivityResult(requestCode, resultCode, data);
+        VKUIHelper.onActivityResult(this,requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VKUIHelper.onDestroy(this);
+    }
+
+    //Слушатель для VKSdk
+    VKSdkListener listener = new VKSdkListener() {
+        @Override
+        public void onCaptchaError(VKError captchaError) {
+            new VKCaptchaDialog(captchaError).show();
+        }
+
+        @Override
+        public void onReceiveNewToken(VKAccessToken newToken) {
+            super.onReceiveNewToken(newToken);
+            //Получение нового токена после авторицации и запуск активности
+            startMainActivity();
+
+        }
+
+        @Override
+        public void onAcceptUserToken(VKAccessToken token) {
+            super.onAcceptUserToken(token);
+            //Точно не понял, но вроде что о хорошее - запускаем активность
+            startMainActivity();
+        }
+
+        @Override
+        public void onTokenExpired(VKAccessToken expiredToken) {
+            //Токен просрочен, запуск авторизации
+            authorize();
+        }
+
+        @Override
+        public void onAccessDenied(VKError authorizationError) {
+            new AlertDialog.Builder(VKUIHelper.getTopActivity())
+                    .setMessage(authorizationError.toString())
+                    .show();
+        }
+    };
+
+    private void authorize(){
+        //Запускаем авторизацию со списком разрешений. Результат - запуск VKOpenAuthActivity
+        VKSdk.authorize(scope,true,false);
+    }
+
+
+    private void startMainActivity(){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+}
--- app/src/main/java/ru/startandroid/vkclient/MainActivity.java
+++ app/src/main/java/ru/startandroid/vkclient/MainActivity.java
@@ -1,9 +1,15 @@
 package ru.startandroid.vkclient;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKUIHelper;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,9 +17,33 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        VKUIHelper.onCreate(this);
         setContentView(R.layout.activity_main);
+        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        VKUIHelper.onResume(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VKUIHelper.onDestroy(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        VKUIHelper.onActivityResult(this,requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +65,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void logout(){
+        VKSdk.logout();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
